08aca51afb90b80e0d8544ff2461f8f6e989e58f
mofas <fedt1234@hotmail.com>
2012-11-16 17:49:24 +0800

    touch event

--- iphoneAPP/js/main.js
+++ iphoneAPP/js/main.js
@@ -8,14 +8,20 @@ $(document).ready(function() {
 
 
 
-
 var youtube = (function(o){
 
-	var $selectMenu ,$openSelectMenuButton;
+	var $el , $mainBoard , $selectMenu ,$openSelectMenuButton;
+
+	var touchInitX = 0 , touchMoveX = 0 , touchDiff;
+	var threshold = 100 , touchTimer;
+
+	var dashBoardOpen = false;
 
 	o.init = function(){
-		$openSelectMenuButton = $(".openSelectMenuButton");
-		$selectMenu = $(".youtube .selectMenu");
+		$el = $(".youtube");
+		$mainBoard = $el.find(".mainBoard");
+		$openSelectMenuButton = $el.find(".openSelectMenuButton");
+		$selectMenu = $el.find(".selectMenu");
 		bindEvent();
 	}
 
@@ -29,6 +35,65 @@ var youtube = (function(o){
 			e.preventDefault();
 			$selectMenu.addClass("hide");
 		});
+
+		$mainBoard[0].addEventListener("touchstart", function(e){			
+			touchInitX = e.changedTouches[0].clientX;
+			touchTimer = setInterval( touchAnimation , 50);
+		});
+
+		$mainBoard[0].addEventListener("touchmove", function(e){
+			touchMoveX = e.changedTouches[0].clientX;			
+			touchDiff = touchMoveX-touchInitX;			
+		}, false);
+
+		$mainBoard[0].addEventListener("touchend", function(e){	
+			clearTimeout(touchTimer);
+			if(touchDiff > threshold && !dashBoardOpen){
+				openDashBoard();
+			}
+			else if(dashBoardOpen && touchDiff < -threshold){
+				closeDashBoard();
+			}
+			else if(dashBoardOpen && touchDiff > -threshold){
+				openDashBoard();
+			}
+			else{
+				closeDashBoard();
+			}
+		});
+
+
+	}
+
+	var touchAnimation = function(){
+		if(!dashBoardOpen){
+			if(touchDiff < 0 )
+				touchDiff = 0;
+			$mainBoard.css({
+				left: touchDiff
+			});
+		}
+		else{
+			$mainBoard.css({
+				left: 280 + touchDiff
+			});
+		}
+		
+	}
+
+
+	var openDashBoard = function(){
+		$mainBoard.animate({
+			left: 280,
+		}, 300 , 'easeOutExpo');
+		dashBoardOpen = true;
+	}
+
+	var closeDashBoard = function(){
+		$mainBoard.animate({
+			left: 0,
+		}, 300 , 'easeOutExpo');
+		dashBoardOpen = false;
 	}
 
 	return o;
