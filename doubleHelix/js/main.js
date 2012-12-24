6c229406b031a72f4a554d8f3bcc48d9843eca8c
mofas <fedt1234@hotmail.com>
2012-12-24 18:04:05 +0800

    doubleHelix v2

--- doubleHelix/js/main.js
+++ doubleHelix/js/main.js
@@ -29,7 +29,10 @@ var doubleHelix = (function(o){
 
 	var amplitude = W/2,
 		phase = 0;
-	
+
+	var particles = [],
+		particleDistance = 20,
+		helixArrayLength;
 
 	var resize = function(){
 		W = window.innerWidth;
@@ -38,72 +41,177 @@ var doubleHelix = (function(o){
 		canvas.height = H;
 		ctx.fillStyle = "white";
 		ctx.fillRect(0,0,W,H);
-		amplitude = W/4;
+		amplitude = W/6;
+		helixArrayLength = Math.floor(H/particleDistance) + 25;
+
+		initParticle();
+		helixUpdate();
+		particlesAnimation();
 	}
 
 
-	var drawHelixLayout = function(){		
+	var rotateFunction = function(originX , originY , rotateAngle){
+		x = originX*Math.cos(rotateAngle) + originY*Math.sin(rotateAngle);
+		y = - originX*Math.sin(rotateAngle) + originY*Math.cos(rotateAngle);
+		return [ x , y ];
+	}
+
+
+	var HelixParticleUpdate = function(){		
+
+		clearCanvas();
 
 		ctx.beginPath();
-		ctx.lineWidth = 1;
-		var originX = 0;
-		var originY = 0;
-		var x;
-		var y;
-		var rotateAngle = 0;
+		ctx.fillStyle = "#000" ;
 
-		ctx.moveTo( 0 , 0);
 
-		for(var i = 0; i < 2*H ; i+= 5){
-			originX = 0 + (Math.sin(i*deltaDegree + phase)*amplitude);
-			originY = i;
-			//rotate 
-			rotateAngle = Math.PI/6;
-			x = originX*Math.cos(rotateAngle) + originY*Math.sin(rotateAngle);
-			y = - originX*Math.sin(rotateAngle) + originY*Math.cos(rotateAngle);
-			ctx.lineTo(x , y);
+		var originX = 0, originY = 0, x , y;
+		var particle;				
+		for(var i = 0; i < helixArrayLength ; i++){
+			particle = particles[i];
+			//the baseLine
+			originX = W/6 + (Math.sin(i*particleDistance*deltaDegree + phase)*amplitude);
+			originY = i*particleDistance;			
+			var rf = rotateFunction(originX , originY , Math.PI/5);
+			x = rf[0];
+			y = rf[1];
+
+			ctx.moveTo(x , y);
+			ctx.arc(x , y , 5 , 0 , Math.PI*2, true);
+			
+			particle.targetX = x;
+			particle.targetY = y;			
 		}
-		ctx.strokeStyle = "#333";		
-				
-		ctx.moveTo(0 , 0);		
+			
+		for(var i =0; i <= helixArrayLength ; i+= 1){
+			particle = particles[helixArrayLength+i];
+			//the baseLine
+			originX = W/6 + (Math.sin(i*particleDistance*deltaDegree + phase + phaseDiff)*amplitude)
+			originY = i*particleDistance;						
+			var rf = rotateFunction(originX , originY , Math.PI/4);			
+			x = rf[0];
+			y = rf[1];		
 
-		for(var i =0; i < 2*H; i+= 6){
-			originX = 0 + (Math.sin(i*deltaDegree + phase + phaseDiff)*amplitude)
-			originY = i;			
-			//rotate 
-			rotateAngle = Math.PI/5;
-			x = originX*Math.cos(rotateAngle) + originY*Math.sin(rotateAngle);
-			y = - originX*Math.sin(rotateAngle) + originY*Math.cos(rotateAngle);
-			ctx.lineTo(x , y);
+			ctx.moveTo(x , y);
+			ctx.arc(x , y , 5 , 0 , Math.PI*2, true);	
+			
+			particle.targetX = x;
+			particle.targetY = y;				
 		}
-		ctx.strokeStyle = "#333";
-		
-		
-		ctx.stroke();		
+
+		ctx.fill();
 
 		phase += phaseVelocity;
 	}
 
 
-	var fadeOut = function(){
-		ctx.fillStyle = "rgba(255,255,255,1)";
+	var clearCanvas = function(){
+		ctx.beginPath();
+		ctx.fillStyle = "#fff";
 		ctx.fillRect(0,0,W,H);
+		ctx.fill();
 	}
 
-	var helixAnimation = function(){
-		fadeOut();
-		drawHelixLayout();
-		setTimeout(helixAnimation , 100);
+	var helixUpdate = function(){		
+		HelixParticleUpdate();
+		setTimeout(helixUpdate , 500);
 	}
 
+
+	var initParticle = function(){
+		for(var i =0; i <= 2*helixArrayLength ; i+= 1){			
+			particles.push({
+				r: Math.random()*2+1,
+				targetX: 0,
+				targetY: 0,
+				x: 0,
+				y: 0,
+				vx : Math.random()*0.8 - 0.4,
+				vy : Math.random()*0.8 - 0.4,
+			});
+		}		
+
+		for(var i = 0; i < helixArrayLength ; i+= 1){
+			//the baseLine
+			originX = W/6 + (Math.sin(i*particleDistance*deltaDegree + phase)*amplitude);
+			originY = i*particleDistance;			
+			var rf = rotateFunction(originX , originY , Math.PI/6);
+			x = rf[0];
+			y = rf[1];
+
+			ctx.moveTo(x , y);
+			ctx.arc(x , y , 5 , 0 , Math.PI*2, true);
+
+			particle = particles[i];
+			particle.x = x + Math.random()*100 - 50;
+			particle.y = y + Math.random()*100 - 50;
+		}		
+		for(var i =0; i <= helixArrayLength ; i+= 1){
+			//the baseLine
+			originX = W/6 + (Math.sin(i*particleDistance*deltaDegree + phase + phaseDiff)*amplitude)
+			originY = i*particleDistance;						
+			var rf = rotateFunction(originX , originY , Math.PI/5);			
+			x = rf[0];
+			y = rf[1];		
+
+			ctx.moveTo(x , y);
+			ctx.arc(x , y , 5 , 0 , Math.PI*2, true);	
+
+			particle = particles[helixArrayLength+i];			
+			particle.x = x + Math.random()*100 - 50;
+			particle.y = y + Math.random()*100 - 50;
+		}
+
+	}
+
+	var particlesAnimation = function(){
+		var acceleration = 0.002;
+		var distance;
+		clearCanvas();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(30, 30, 30, 1)";
+		var particle;		
+		for(var i = 0 ; i < particles.length ; i++){			
+			particle = particles[i];
+			
+			//calculate velocity			
+			distance = Math.abs(particle.x - particle.targetX)/100;
+
+			if(i === 0)	{
+				console.log(distance);
+			}
+			if( particle.x >= particle.targetX){
+				particle.vx -= acceleration*distance;
+			}
+			if( particle.x <= particle.targetX){
+				particle.vx += acceleration*distance;
+			}
+
+			distance = Math.abs(particle.y - particle.targetY)/100;
+			if( particle.y >= particle.targetY){
+				particle.vy -= acceleration*distance;
+			}			
+			if( particle.y <= particle.targetY){
+				particle.vy += acceleration*distance;
+			}
+			
+			//calculate next position
+			particle.x = particle.x + particle.vx;
+			particle.y = particle.y + particle.vy;
+
+			ctx.moveTo(particle.x , particle.y);
+			ctx.arc(particle.x , particle.y , particle.r , 0 , Math.PI*2, true);
+		}
+		ctx.fill();
+		requestAnimFrame(particlesAnimation);
+	}
 
 	o.init = function(){
 		canvas = document.getElementById("canvas");
 		ctx = canvas.getContext("2d");		
-		//$(window).on("resize" , resize);
-		resize();		
-		helixAnimation();
-		//drawHelixLayout();
+		$(window).on("resize" , resize);
+		resize();
+
 	}
 
 	return o;
