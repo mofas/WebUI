11e951028535af5c8adfb6e7f454620534a16c48
mofas <fedt1234@hotmail.com>
2012-12-26 11:06:40 +0800

    add grid line

--- sphere/js/main.js
+++ sphere/js/main.js
@@ -25,7 +25,7 @@ var sphere = (function(o){
 	var particles = [];
 
 	var warpLineNumber = 16,
-		weftLineNumber = 120;	
+		weftLineNumber = 60;	
 
 
 	var RADIAN = Math.PI*2/360;
@@ -52,7 +52,6 @@ var sphere = (function(o){
 			latitudeAngle = (i+1)*(180/(warpLineNumber+1));			
 			for(var j = 0; j < weftLineNumber ; j++){
 				longtitudeAngle = j*(360/weftLineNumber);
-
 				var x = radius*Math.sin(RADIAN*latitudeAngle)*Math.sin(RADIAN*longtitudeAngle);
 				var y = radius*Math.sin(RADIAN*latitudeAngle)*Math.cos(RADIAN*longtitudeAngle);
 				var z = radius*Math.cos(RADIAN*latitudeAngle);				
@@ -62,6 +61,8 @@ var sphere = (function(o){
 					z: z,
 					lati: latitudeAngle,
 					long: longtitudeAngle,
+					projectX : 0,
+					projectY : 0
 				});
 			}			
 		}		
@@ -69,6 +70,39 @@ var sphere = (function(o){
 	}
 
 	var drawParticlesOnCanvas = function(){
+
+		//project the point from 3d to 2d plane 
+		//point [x , y , z]
+		//camera [x , y , z]
+		//orientationAngle = camera orientation [x , y , z]
+		//position [x , y , z]
+		var projectFunction = function(point , camera , orientationAngle , position){
+			var cos = Math.cos;
+			var sin = Math.sin;
+			var ax = point.x || 0,
+				ay = point.y || 0,
+				az = point.z || 0,
+				cx = camera.x || 0,
+				cy = camera.y || 0,			
+				cz = camera.z || 0, 
+				ox = orientationAngle.x*RADIAN || 0,
+				oy = orientationAngle.y*RADIAN || 0,
+				oz = orientationAngle.z*RADIAN || 0,
+				ex = position.x || 0,
+				ey = position.y || 0,
+				ez = position.z || 0;
+
+			var dx , dy , dz;
+			dx = cos(oy)*(sin(oz)*(ay-cy) + cos(oz)*(ax-cx)) - sin(oy)*(az-cz);
+			dy = sin(ox)*(cos(oy)*(az-cz) + sin(oy)*(sin(oz)*(ay-cy)+cos(oz)*(ax-cx))) + cos(ox)*(cos(oz)*(ay-cy)-sin(oz)*(ax-cx));
+			dz = cos(ox)*(cos(oy)*(az-cz) + sin(oy)*(sin(oz)*(ay-cy)+cos(oz)*(ax-cx))) - sin(ox)*(cos(oz)*(ay-cy)-sin(oz)*(ax-cx));
+			
+			var bx = (dx-ex)*(ez/dz),
+				by = (dy-ey)*(ez/dz);
+
+			return [bx , by];
+		}
+
 		//project particles on plane		
 		var particle;
 		for(var i =0; i < particles.length ;i++){
@@ -80,54 +114,58 @@ var sphere = (function(o){
 			var pf = projectFunction(particle , camera , orientationAngle , position);
 			var x = pf[0],
 				y = pf[1];
+
+			//TEST CODE
 			/**
 			var x = particle.x + W/2, 
 				y = particle.z + H/2;
 			**/
-			console.log(x , y);
+			particle.projectX = x;
+			particle.projectY = y;			
 			ctx.moveTo(x , y);
-			var color = "rgba(" + ((particle.x/radius)*50+50) + "%," + ((particle.y/radius)*50+50) + "%," + ((particle.z/radius)*50+50) + "% , 0.4)";			
+			//var color = "rgba(" + ((particle.x/radius)*50+50) + "%," + ((particle.y/radius)*50+50) + "%," + ((particle.z/radius)*50+50) + "% , 0.4)";
+			var color = "#999";
 			ctx.fillStyle = color;			
 			ctx.arc(x , y , 2 , 0 , Math.PI*2, true);
 			ctx.fill();
-		}			
+		}
+
+		drawGridLine();	
 	}
 
 
-	//project the point from 3d to 2d at plane 
-	//point [x , y , z]
-	//camera [x , y , z]
-	//orientationAngle = camera orientation [x , y , z]
-	//position [x , y , z]
-	var projectFunction = function(point , camera , orientationAngle , position){
-
-		var cos = Math.cos;
-		var sin = Math.sin;
-		var ax = point.x || 0,
-			ay = point.y || 0,
-			az = point.z || 0,
-			cx = camera.x || 0,
-			cy = camera.y || 0,			
-			cz = camera.z || 0, 
-			ox = orientationAngle.x*RADIAN || 0,
-			oy = orientationAngle.y*RADIAN || 0,
-			oz = orientationAngle.z*RADIAN || 0,
-			ex = position.x || 0,
-			ey = position.y || 0,
-			ez = position.z || 0;
-
-
-		var dx , dy , dz;
-
-		dx = cos(oy)*(sin(oz)*(ay-cy) + cos(oz)*(ax-cx)) - sin(oy)*(az-cz);
-		dy = sin(ox)*(cos(oy)*(az-cz) + sin(oy)*(sin(oz)*(ay-cy)+cos(oz)*(ax-cx))) + cos(ox)*(cos(oz)*(ay-cy)-sin(oz)*(ax-cx));
-		dz = cos(ox)*(cos(oy)*(az-cz) + sin(oy)*(sin(oz)*(ay-cy)+cos(oz)*(ax-cx))) - sin(ox)*(cos(oz)*(ay-cy)-sin(oz)*(ax-cx));
-
+	var drawGridLine = function(){
+		var p0 , p;
+		//draw latitude
 		
-		var bx = (dx-ex)*(ez/dz),
-			by = (dy-ey)*(ez/dz);
+		ctx.beginPath();
+		ctx.strokeStyle = "rgba(170,170,170,1)";
+		ctx.lineWidth = 0.5;
+		for(var i = 0; i < warpLineNumber ; i++){				
+			p0 = particles[i*weftLineNumber];
+			ctx.moveTo(p0.projectX, p0.projectY);			
+			for(var j = 0; j < weftLineNumber ; j++){
+				p = particles[i*weftLineNumber+j];
+				ctx.lineTo(p.projectX, p.projectY);
+			}
+			ctx.lineTo(p0.projectX, p0.projectY);
+		}		
+		ctx.stroke();		
 
-		return [bx , by];
+		//draw longitude
+		ctx.beginPath();
+		ctx.strokeStyle = "rgba(170,170,170,1)";
+		ctx.lineWidth = 0.5;
+		for(var i = 0; i < weftLineNumber ; i++){				
+			p0 = particles[i];
+			ctx.moveTo(p0.projectX, p0.projectY);			
+			for(var j =0; j < warpLineNumber ; j++){
+				p = particles[i + weftLineNumber*j];
+				ctx.lineTo(p.projectX, p.projectY);
+			}
+		}		
+		ctx.stroke();
+
 	}
 
 
